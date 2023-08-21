711e03e7d60694424929fdd2d56bee92f08bc7be
嘉 <140303219+2535081041@users.noreply.github.com>
2023-08-21 21:29:48 +0800

    Update myconfig.py

--- rearserver/myapp/myconfig.py
+++ rearserver/myapp/myconfig.py
@@ -7,16 +7,12 @@ import urllib.parse
 import ssl
 
 # todo 设置ssl的传输文件
-SSL_CONTEXT = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-SSL_CONTEXT.set_ciphers('ECDHE-RSA-AES128-GCM-SHA256:HIGH:!aNULL:!MD5:!RC4:!DHE')   # 传输算法
-SSL_CONTEXT.load_cert_chain('/project/ssl_proof/cxjweb.top_bundle.crt', '/project/ssl_proof/cxjweb.top.key')    # 证书
-
 
 # todo 数据库相关配置
 DATABASE_HOSTNAME = "127.0.0.1"  # 数据库ip
 DATABASE_PORT = 3306  # 数据库端口
-quoted_username = urllib.parse.quote_plus('root')  # 用户
-quoted_password = urllib.parse.quote_plus("Cxj20011019!")  # 密码
+quoted_username = urllib.parse.quote_plus('账号')  # 用户
+quoted_password = urllib.parse.quote_plus("密码")  # 密码
 tem_DATABASE = 'tem_xiaomeng_db'  # 测试环境数据库名称
 DATABASE = 'xiaomeng_db'  # 生产数据库名称
 
