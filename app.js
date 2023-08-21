3ad47e4f81ade945a6b4530b46437f0941b56b8d
嘉 <140303219+2535081041@users.noreply.github.com>
2023-08-21 20:54:03 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+
+// app.js
+App({
+  onLaunch() {
+  },
+
+  globalData: {
+    path_: "https://cxjweb.top/XM/TY",
+    img_root: "https://cxjweb.top/XM/img/TY",
+
+    // 用户的收藏列表
+    item_id_collect_list:[],
+
+    // 缓存的帖子数据
+    item_ids:[],
+    items:{},
+
+    // 校历
+    semester_info:'',
+
+    // 缓存数据
+    id:'',
+    openid:'',
+    name:'',
+    course:'',
+    stu_id:'',
+    photo_path:'',
+
+  }
+})
